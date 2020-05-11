11ed3c00d8196ce7ea4db3975ad7883acb44aae7
thiagocardosoca <thiago.cardoso@contaazul.com>
2020-05-11 13:12:13 -0300

    adding prolog base

--- covid-19.py
+++ covid-19.py
@@ -1,5 +1,7 @@
 from pyswip import Prolog
 
-prolog = Prolog()
 
+prolog = Prolog()
 prolog.consult("covid-19_base.pl")
+
+print(list(prolog.query("very_serious_case_covid.")))
--- covid-19_base.pl
+++ covid-19_base.pl
@@ -1,85 +1,62 @@
-informacao :-
-    write('covid19 - Sistema Especialista'),
+infPatient(glaucoLindo, 24, 90, 16, 110, 98, 0, 24, 0).
+infPatient(jairson, 36, 90, 16, 110, 97, 0, 24, 0).
+infPatient(jubilou, 40, 111, 28, 113, 98, 1, 63, 1).
+infPatient(jubilino, 24, 90, 16, 110, 95, 1, 24, 1).
+infPatient(jabulinare, 41, 127, 32, 87, 94, 1, 24, 2).
+  
+ligh_case_covid :-
+    infPatient(N, T, Bpm, Pa, Pas, Sa, Dis, Age, Com),
+               
+    T =< 37,
+    T >= 35,
+    Bpm < 100,
+    Pa < 18,
+    Pas > 100,
+    Sa >= 95,
+    Dis =:= 0,
+    Age < 60,
+    Com < 1,
+    
+    write('Light case: You must stay at home, under observation for 14 days!'),
+    format('\n name: ~w \t temperature: ~d \t bpm: ~d \t pa: ~d \t pas: ~d \t sa: ~d \t dispine: ~w \t age: ~d \t comorbidities: ~d',[N, T, Bpm, Pa, Pas, Sa, Dis, Age, Com]),
     nl.
 
-% Todas gravidades possíveis com os seus critérios
-gravidade(Sintoma, gravissimo) :-
-    sintomas(Sintoma, temperatura_acima_de_39), % > 39
-    sintomas(Sintoma, frequencia_respiratoria_maior_que_30), % > 30
-    sintomas(Sintoma, pa_sistolica_menor_que_90), % < 90
-    sintomas(Sintoma, sa_O2_menor_que_95), % < 95
-    sintomas(Sintoma, dispneia_sim). % true
+average_case_covid :-
+    infPatient(N, T, Bpm, Pa, Pas, Sa, Dis, Age, Com),
+               
+    (T < 35; T >=37, T =<39),
+    Bpm > 100,
+    Pa > 19, 
+    Pa =< 30,
+    Age >= 60,
+    Age =< 79,
+    Com =:= 1,
+    
+    write('Light case: You must stay at home, under observation for 14 days'),
+    format('\n name: ~w \t temperature: ~d \t bpm: ~d \t pa: ~d \t pas: ~d \t sa: ~d \t dispine: ~w \t age: ~d \t comorbidities: ~d',[N, T, Bpm, Pa, Pas, Sa, Dis, Age, Com]),
+    nl.
 
-gravidade(Sintoma, grave) :-
-    sintomas(Sintoma, temperatura_acima_de_39), % > 39
-    sintomas(Sintoma, pa_sistolica_entre_90_e_100),
-    sintomas(Sintoma, idade_maior_igual_a_80),
-    sintomas(Sintoma, comorbidades_igual_a_2_ou_mais).
+serious_cases_covid :-
+    infPatient(N, T, Bpm, Pa, Pas, Sa, Dis, Age, Com),
+               
+	T >= 39,
+    Pas >= 90,
+    Pas =< 100,
+    Age > 80,
+    Com >= 2,
 
-gravidade(Sintoma, medio) :-
-    sintomas(Sintoma, temperatura_menor_que_35_e_entre_37_e_39),
-    sintomas(Sintoma, frequencia_cardiaca_maior_que_100),
-    sintomas(Sintoma, frequencia_respiratoria_entre_19_e_30),
-    sintomas(Sintoma, idade_entre_60_e_79),
-    sintomas(Sintoma, comorbidades_igual_a_1).
+    write('Serious case: Should be urgently referred to the hospital'),
+    format('\n name: ~w \t temperature: ~d \t bpm: ~d \t pa: ~d \t pas: ~d \t sa: ~d \t dispine: ~w \t age: ~d \t comorbidities: ~d',[N, T, Bpm, Pa, Pas, Sa, Dis, Age, Com]),
+    nl.
 
-gravidade(Sintoma, leve) :-
-    sintomas(Sintoma, temperatura_entre_35_e_37),
-    sintomas(Sintoma, frequencia_cardiaca_menor_que_100),
-    sintomas(Sintoma, frequencia_respiratoria_menor_que_18),
-    sintomas(Sintoma, pa_sistolica_maior_que_100),
-    sintomas(Sintoma, sa_O2_maior_igual_que_95),
-    sintomas(Sintoma, dispneia_nao),
-    sintomas(Sintoma, idade_menor_que_60),
-    sintomas(Sintoma, comorbidades_igual_a_0).
+very_serious_case_covid :-
+    infPatient(N, T, Bpm, Pa, Pas, Sa, Dis, Age, Com),
+               
+    Pa > 30,
+    Pas < 90,
+    Sa < 95,
+    Dis =:= 1,
 
-% Obtém sintoma e faz a pergunta
-sintomas(Sintoma, Valor) :- pergunta('O paciente possui estado de gravidade ', Valor).
-
-% Cria cache (verifica se pergunta já foi perguntada e continua)
-pergunta(PerguntaFixa, Valor) :- cache(PerguntaFixa, Valor, true).
-
-% Cria cache (verifica se pergunta já foi perguntada e caso já tenha sido segue para a próxima pergunta)
-pergunta(PerguntaFixa, Valor) :- cache(PerguntaFixa, Valor, false), fail.
-
-% Faz a pergunta, obtém a resposta, valida no cache verdadeuro ou (;) falso e continua
-pergunta(PerguntaFixa, Valor) :-
-    nl,
-    write(PerguntaFixa),
-    write(Valor),
-    write('? (s/n)'),
-    read(Ans),
-    (
-        (Ans=s, assert(cache(PerguntaFixa, Valor, true)));
-        (assert(cache(PerguntaFixa, Valor, false)), fail)
-    ).
-
-% Executa as perguntas
-resultado :-
-    nl,
-    gravidade(sintomas, Gravidade),
-    nl,
-    write('O paciente possui estado de gravidade '),
-    write(Gravidade).
-
-% Exibe mensagem de não possui gravidade
-resultado :-
-    nl,
-    write('Não possui gravidade').
-
-% Exibe informação do programa, limpa as funções (abolish),
-% informa que o cache pode mudar durante a compilação (dynamic), remove todos valores de dynamic
-% e executa o resultado... se tentar novamento for verdadeiro repete de novo.
-inicio :-
-    informacao,
-    repeat,
-    abolish(cache/3),
-    dynamic(cache/3),
-    retractall(cache/3),
-    resultado,
-    nl, nl,
-    write('Tentar novamente ? (s/n)'),
-    read(Resposta),\+ Resposta=s,
-    nl,
-    write('Encerrado!'),
-    abolish(cache,3).
+    write('Serious case: Should be urgently referred to the hospital'),
+    format('\n name: ~w \t temperature: ~d \t bpm: ~d \t pa: ~d \t pas: ~d \t sa: ~d \t dispine: ~w \t age: ~d \t comorbidities: ~d',[N, T, Bpm, Pa, Pas, Sa, Dis, Age, Com]),
+    nl.
